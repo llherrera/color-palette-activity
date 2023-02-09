09c9540759d2003e39efe6ecacbdb282c10a25b7
Leonardo <leonardoh050@gmail.com>
2023-02-09 12:44:08 -0500

    create palette without bottons

--- lib/ui/pages/color_selection_page.dart
+++ lib/ui/pages/color_selection_page.dart
@@ -25,9 +25,71 @@ class ColorSelectionPage extends StatelessWidget {
           ],
         ),
         // https://colorhunt.co/
-        body: (const Center(
-          child: Text(
-              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),
+        body: (Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [ColorPalette(
+                baseColor1: 'F6F7C1',
+                callback1: showColor,
+                baseColor2: 'C1AEFC',
+                callback2: showColor,
+                baseColor3: 'D1FFF3',
+                callback3: showColor,
+                baseColor4: 'BEF0CB',
+                callback4: showColor,
+              ), ColorPalette(
+                baseColor1: '20262E',
+                callback1: showColor,
+                baseColor2: 'E9E8E8',
+                callback2: showColor,
+                baseColor3: 'CD5888',
+                callback3: showColor,
+                baseColor4: '913175',
+                callback4: showColor,
+              ),ColorPalette(
+                baseColor1: '5D3891',
+                callback1: showColor,
+                baseColor2: 'F5F5F5',
+                callback2: showColor,
+                baseColor3: 'E8E2E2',
+                callback3: showColor,
+                baseColor4: 'F99417',
+                callback4: showColor,
+              ), ColorPalette(
+                baseColor1: 'F2CD5C',
+                callback1: showColor,
+                baseColor2: '400E32',
+                callback2: showColor,
+                baseColor3: 'A61F69',
+                callback3: showColor,
+                baseColor4: 'F2921D',
+                callback4: showColor,
+              ),ColorPalette(
+                baseColor1: '03C988',
+                callback1: showColor,
+                baseColor2: '13005A',
+                callback2: showColor,
+                baseColor3: '00337C',
+                callback3: showColor,
+                baseColor4: '1C82AD',
+                callback4: showColor,
+              ), ColorPalette(
+                baseColor1: 'EBC7E6',
+                callback1: showColor,
+                baseColor2: '645CBB',
+                callback2: showColor,
+                baseColor3: 'A084DC',
+                callback3: showColor,
+                baseColor4: 'BFACE2',
+                callback4: showColor,
+              ),
+              ],
+            )
+          ),
+          
+          
+          /*child: Text(
+              "SingleChildScrollView -> Column -> [ColorPalette, ColorPalette] "),*/
         )));
   }
 
--- lib/ui/widgets/color_palette.dart
+++ lib/ui/widgets/color_palette.dart
@@ -5,15 +5,28 @@ class ColorPalette extends StatelessWidget {
   const ColorPalette({
     Key? key,
     // así se reciben en el constructor, por ejemplo
-    //required this.baseColor,
+    required this.baseColor1,
+    required this.baseColor2,
+    required this.baseColor3,
+    required this.baseColor4,
+    required this.callback1,
+    required this.callback2,
+    required this.callback3,
+    required this.callback4,
   }) : super(key: key);
 
   // atributos de la clase, por ejemplo
-  //final String baseColor;
+  final String baseColor1;
+  final String baseColor2;
+  final String baseColor3;
+  final String baseColor4;
 
   // también se puede tener como un atributo un callback, necesiario ya que es un
   // widget sin estado, por ejemplo
-  //final Function(String) callback;
+  final Function(String) callback1;
+  final Function(String) callback2;
+  final Function(String) callback3;
+  final Function(String) callback4;
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +34,48 @@ class ColorPalette extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       // vamos a mostrar los elementos en un row
-      child: Text(
-          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),
+      child: 
+        Row(children: <Widget> [
+          Expanded(
+            flex:5,
+            child: GestureDetector( 
+              child: Container(
+                height: 100, 
+                color: ColorUtils.FromHex(baseColor1),
+              ),
+            ),
+          ),
+          Expanded(
+            flex:4,
+            child: GestureDetector( 
+              child: Container(
+                height: 100, 
+                color: ColorUtils.FromHex(baseColor2),
+              ),
+            ),
+          ),
+          Expanded(
+            flex:3,
+            child: GestureDetector( 
+              child: Container(
+                height: 100, 
+                color: ColorUtils.FromHex(baseColor3),
+              ),
+            ),
+          ),
+          Expanded(
+            flex:2,
+            child: GestureDetector( 
+              child: Container(
+                height: 100, 
+                color: ColorUtils.FromHex(baseColor4),
+              ),
+            ),
+          ),
+        ],)
+      
+      /*Text(
+          'Row -> [Expanded(flex: 4,GestureDetector(Container(height: 100, color: ColorUtils.FromHex(baseColor))) )]'),*/
     );
   }
 }
